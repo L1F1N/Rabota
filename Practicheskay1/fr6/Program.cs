f00f83997f80d3a3b923d8883356ce20b0269c8f
L1F1N <118181137+L1F1N@users.noreply.github.com>
2023-04-11 12:05:48 +0300

    Add files via upload

---
+++ Practicheskay1/fr6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fr6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///double c = Convert.ToInt32(Console.ReadLine());
+            double d = Convert.ToDouble(Console.ReadLine());
+            ///Console.WriteLine(Convert.ToInt32(Math.Abs(c)));
+            double c;
+            c = 3.14 * Math.Pow(d, 2);
+            Console.WriteLine(Math.Abs(c));
+
+        }
+    }
+}
