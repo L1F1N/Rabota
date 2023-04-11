ed6d28c1a47649106fd66f6b9006a3db1b18dc15
L1F1N <118181137+L1F1N@users.noreply.github.com>
2023-04-11 13:28:49 +0300

    Add files via upload

---
+++ Practicheskaya4/Practicheskaya4/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicheskaya4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char A_1 = Convert.ToChar(Console.ReadLine());
+            switch (A_1)
+            {
+                case 'ф':
+                    A_1 = 'ц';
+                    Console.WriteLine(A_1);
+                    break;
+                case 'ы':
+                    A_1 = 'у';
+                    Console.WriteLine(A_1);
+                    break;
+                case 'в':
+                    A_1 = 'к';
+                    Console.WriteLine(A_1);
+                    break;
+                case 'а':
+                    A_1 = 'е';
+                    Console.WriteLine(A_1);
+                    break;
+                case 'п':
+                    A_1 = 'н';
+                    Console.WriteLine(A_1);
+                    break;
+                case 'р':
+                    A_1 = 'г';
+                    Console.WriteLine(A_1);
+                    break;
+                case 'о':
+                    A_1 = 'ш';
+                    Console.WriteLine(A_1);
+                    break;
+                case 'л':
+                    A_1 = 'щ';
+                    Console.WriteLine(A_1);
+                    break;
+                case 'д':
+                    A_1 = 'з';
+                    Console.WriteLine(A_1);
+                    break;
+                case 'ж':
+                    A_1 = 'х';
+                    Console.WriteLine(A_1);
+                    break;
+                case 'э':
+                    A_1 = 'ъ';
+                    Console.WriteLine(A_1);
+                    break;
+                case 'ё':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'Ё':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'й':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'ц':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'у':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'к':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'е':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'н':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'г':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'ш':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'щ':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'з':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'х':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'ъ':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'Й':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'Ц':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'У':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'К':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'Е':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'Н':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'Г':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'Ш':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'Щ':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'З':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'Х':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+                case 'Ъ':
+                    Console.WriteLine("Для этого символа нет символа расположеного справа сверху");
+                    break;
+            }
+        }
+    }
+}
